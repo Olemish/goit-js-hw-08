d2de2ba98a8215e6e2055b93f61244949d65927a
Olemish <olesia.hlushenkova@gmail.com>
2022-06-13 00:39:36 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector(".feedback-form input")
+const message = document.querySelector(".feedback-form textarea")
+
+const state = localStorage.getItem('feedback-form-state') ? JSON.parse(localStorage.getItem('feedback-form-state')) : {}
+
+if (localStorage.getItem("feedback-form-state") !== null) {
+    const { email, message } = JSON.parse(localStorage.getItem('feedback-form-state'))
+    form.email.value = email;
+    form.message.value = message;
+}
+
+email.addEventListener('input', e => handleInput(e))
+message.addEventListener('input', e => handleInput(e))
+form.addEventListener('submit', e => clearSubmitForm(e))
+
+function handleInput(e) {
+    state[e.target.name]= e.target.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(state))
+}
+
+function clearSubmitForm(e){
+    e.preventDefault()
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')))  
+    localStorage.removeItem('feedback-form-state')
+    form.email.value = "";
+    form.message.value = "";
+}
 
